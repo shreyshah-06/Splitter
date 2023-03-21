f7d1321a4784027e7468de8e688b5d33134659a7
Shrey Shah <shrey.shah0610@gmail.com>
2023-03-21 16:59:01 +0530

    Splitter App

---
+++ client/src/components/Copyright.jsx
@@ -0,0 +1,13 @@
+import { Link, styled, Typography } from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+
+export default function Copyright() {
+
+  return (
+    <Typography mt="-2" variant="body2" align="center" sx={{ color: 'text.secondary'}}>
+            &copy; Shrey Shah
+            <br/>
+          </Typography>
+  )
+}
